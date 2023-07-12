237d6d9247fac67771f817da5c0f33adfba6c640
sohseyedi <sohseyedi@gmail.com>
2023-07-12 16:57:13 +0330

    style footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/home/Layout";
 import Cart from "./components/cart/Cart";
 import Single from "./components/singlepage/Single";
+import * as BiIcon from "react-icons/bi";
 import Products from "./components/products/Products";
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
         <Route element={<Single />} path={"/products/:id"} />
         <Route element={<Products />} path={"/products"} />
       </Routes>
+      <div className="contact">
+        <BiIcon.BiMessageDetail size={30}/>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/home/footer/Footer.jsx
+++ src/components/home/footer/Footer.jsx
@@ -1,9 +1,86 @@
-import './Footer.scss'
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+import enamad from "../../../assets/enamad.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container container">
+        <div className="footer-container__details">
+          <div className="footer-container__details-title">
+            ضمانت <span>سبز لرن</span> برای کلیه محصولات آموزشی
+          </div>
+          <p>
+            وقتی تازه شروع به یادگیری برنامه نویسی کردم. یکی از مشکلاتی که در
+            فرآیند یادگیری داشتم، کمبود آموزش های خوب با پشتیبانی قابل قبول بود
+            که باعث شد اون موقع تصمیم بگیرم اگر روزی توانایی مالی و فنی قابل
+            قبولی داشتم یک وب سایت برای حل این مشکل راه اندازی کنم! و خب امروز
+            آکادمی آموزش برنامه نویسی <span>سبز لرن</span> به عنوان یک آکادمی
+            خصوصی فعالیت میکنه و این به این معنی هست که هر مدرسی اجازه تدریس در
+            اون رو نداره و باید از فیلترینگ های خاص آکادمی <span>سبز لرن</span>{" "}
+            رد شه! این به این معنی هست که ما برامون فن بیان و نحوه تعامل مدرس با
+            دانشجو بسیار مهمه! ما در آکادمی <span>سبز لرن</span> تضمین پشتیبانی
+            خوب و با کیفیت رو به شما میدیم . چرا که مدرسین وب سایت{" "}
+            <span>سبز لرن</span> حتی برای پشتیبانی دوره های رایگان شون هم مثل
+            آموزش جاوا اسکریپت هزینه دریافت میکنند و متعهد هستند که هوای کاربر
+            های عزیز رو داشته باشند
+          </p>
+        </div>
+        <div className="footer-container__items">
+          <div className="footer-container__items-line">
+            <Link to="/" class="footer-content__items-link">
+              فروشگاه
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              حساب کاربری
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              درباره ما
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              تدریس
+            </Link>
+          </div>
+          <div className="footer-container__items-line">
+            <Link to="/" class="footer-content__items-link">
+              مقالات
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              قوانین و مقررات
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              آموزش جاوا اسکریپت
+            </Link>
+            <Link to="/" class="footer-content__items-link">
+              آموزش گیت
+            </Link>
+          </div>
+          <div className="footer-container__items-line">
+            <h6>ساعات کاری:</h6>
+            <p>
+              شنبه تا چهارشنبه 8 صبح تا 18 عصر
+              <br />
+              پنج شنبه‌ها ساعت 8 صبح تا 12 ظهر
+            </p>
+          </div>
+          <div className="footer-container__items-line">
+            <h6> ارتباط با ما:</h6>
+            <p>
+              ایمیل : sabzlearn@gmail.com
+              <br />
+              شماره تماس : 09334008385
+            </p>
+          </div>
+          <div className="footer-container__items-line">
+            <img src={enamad} alt="" />
+          </div>
+        </div>
+        <p className="footer-container__copy">
+          © کلیه حقوق برای <span>سبز لرن</span> محفوظ است ۱۴۰۱
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
