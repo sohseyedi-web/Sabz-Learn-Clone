34041d03a35d91507847684255cb3300115d43ac
sohseyedi <sohseyedi@gmail.com>
2023-07-12 20:47:06 +0330

    style products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,22 +1,11 @@
 import "./Products.scss";
-import dataItem from "../../data/dataItem";
-import ProductCard from "../common/ProductCard/ProductCard";
-import Sidebar from "./sidebar/Sidebar";
-import Navbar from "../../container/navbar/Navbar";
+
 
 const Products = () => {
   return (
-    <>
-      <Navbar />
-      <section className="products">
-        <Sidebar />
-        <div className="products-lists">
-          {dataItem.map((item) => (
-            <ProductCard item={item} key={item.id} />
-          ))}
-        </div>
-      </section>
-    </>
+    <Container>
+      <div>فعلا محصولی نیست</div>
+    </Container>
   );
 };
 
